bc80e4789fb1ff6fd35545890519be3cfbd2625a
GULDENER Camille <98022298+GuldenerCamille@users.noreply.github.com>
2024-05-28 17:57:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,22 +21,22 @@ with col1:
         col11, col12 = st.columns((1, 1), gap="medium")
 
         with col11:
-            Intake_Type = st.text_input(label="**Intake Type :**")
-            Intake_Condition = st.text_input(label="**Intake Condition :**")
-            Animal_Type = st.text_input(label="**Animal Type :**")
+            Intake Type = st.text_input(label="**Intake Type :**")
+            Intake Condition = st.text_input(label="**Intake Condition :**")
+            Animal Type = st.text_input(label="**Animal Type :**")
 
         with col12:
-            Sex_upon_Intake = st.text_input(label="**Sex upon Intake :**")
+            Sex upon Intake = st.text_input(label="**Sex upon Intake :**")
             Age_Upon_Intake_Days = st.number_input(label="**Age in days :**", step=1)
             Animal_Group = st.text_input(label="**Animal Group :**")
 
         model_data_dict = {
-            "Intake_Type": Intake_Type,
-            "Intake_Condition": Intake_Condition,
-            "Animal_Type": Animal_Type,
-            "Sex_upon_Intake": Sex_upon_Intake,
-            "Animal_Group": Animal_Group,
-            "Age_Upon_Intake_Days": Age_Upon_Intake_Days
+            "Intake Type": Intake Type,
+            "Intake Condition": Intake Condition,
+            "Animal Type": Animal Type,
+            "Sex upon Intake": Sex upon Intake,
+            "Age_Upon_Intake_Days": Age_Upon_Intake_Days,
+            "Animal_Group": Animal_Group
         }
 
         model_data_df = pd.DataFrame(model_data_dict, index=[0])
